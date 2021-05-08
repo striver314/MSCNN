e399deef9bbb415905b84c4c58f7c5f897f963bc
Guicai Xie <1608858349@qq.com>
2021-05-08 12:57:33 +0800

    Update fullconnect.py
    modify the source code of Batch_Net

--- fullconnect.py
+++ fullconnect.py
@@ -47,8 +47,8 @@ class Batch_Net(nn.Module):
 
     def __init__(self, in_dim, n_hidden_1,  n_hidden_2, out_dim):
         super(Batch_Net, self).__init__()
-        self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden_1),  nn.ReLU(True))
-        self.layer2 = nn.Sequential(nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden_1), nn.BatchNorm1d(n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(nn.Linear(n_hidden_1, n_hidden_2), nn.BatchNorm1d(n_hidden_2), nn.ReLU(True))
         self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, out_dim))
 
     def forward(self, x):
